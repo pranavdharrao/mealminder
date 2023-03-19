563967ee439ee7b93cd8a422344c6a276fbb501d
Pranav Dharrao <programinggeek07@gmail.com>
2023-03-19 12:07:22 +0530

    Added React Router for client-side routing

---
+++ src/static/about.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div className='container'>
+      <h2>About</h2>
+      <p className="my-3"><strong>"MealMinder"</strong> is a made-up name for a web application,
+       but the word "minder" typically means to be attentive to or take care of something.
+       Therefore, "MealMinder" could be interpreted as an application that helps users keep track of their meals
+        or reminds them to eat at regular intervals. It could also imply that the application is designed to help
+        users be more mindful of their eating habits and make healthier choices.</p>
+    </div>
+  )
+}
+
+export default About;
